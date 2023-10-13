a29764ae62d34e0b2c9217b4e310430641e49d10
Thales Luiz Costa <thalesluizc@gmail.com>
2023-10-12 22:33:12 -0300

    Adicionei um arquivo de exercício da BYU

---
+++ shoppingcart-actiivity.py
@@ -0,0 +1,31 @@
+shopping_cart = []
+
+while True:
+    # Exibe o menu de opções
+    print("\nMenu:")
+    print("1. Add to shopping cart")
+    print("2. See itens added")
+    print("3. Exit")
+    
+    choose = input("Choose an option: ")
+    
+    if choose == "1":
+
+        item = input("Enter the name of the item you want to add to the cart: ")
+        shopping_cart.append(item)
+        print(f"{item} has been added to cart.")
+    elif choose == "2":
+
+        if shopping_cart:
+            print("\nItems in cart:")
+            for item in shopping_cart:
+                print("- " + item)
+        else:
+            print("The cart is empty.")
+    elif choose == "3":
+
+        print("Thank you for using the shopping cart. Check back often!")
+        break
+    else:
+
+        print("Invalid option. Please choose a valid option.")
