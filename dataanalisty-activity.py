e7427b87f20a8d52b2c92da1c9c892f3fb729d4d
Thales Luiz Costa <thalesluizc@gmail.com>
2023-10-19 15:47:38 -0300

    ...

--- dataanalisty-activity.py
+++ dataanalisty-activity.py
@@ -1,35 +1,18 @@
+# Lê os dados do arquivo
+with open('dados.txt', 'r') as archieve:
+    linhas = archieve.readlines()
 
-with open('cities.txt') as archive:
-    # Lê as linhas do arquivo
-    lines = archive.readlines()
+# Converte as linhas para números inteiros
+ages = [int(linha.strip()) for linha in linhas]
 
-# Inicializa as variáveis para os valores mais altos e mais baixos
-higher_value = 0  # Inicializado com um valor negativo infinito
-lower_value = 1000000000  # Inicializado com um valor positivo infinito
+# Calcula a média das idades
+average_age = sum(ages) / len(ages)
 
-# Itera pelas linhas do arquivo
-for line in lines:
-    # Divide a linha em partes usando espaço como delimitador (altere para o delimitador correto)
-    parts = line.split()
-    
-    # Trata a primeira linha (se necessário)
-    if lines.index(line) == 0:
-        # Trate a primeira linha aqui
-        pass
-    
-    # Itera pelas partes da linha
-    for part in parts:
-        # Converte a parte para um número (assumindo que são números)
-        number = float(part)
-        
-        # Atualiza o valor mais alto, se necessário
-        if number > higher_value:
-            higher_value = number
-        
-        # Atualiza o valor mais baixo, se necessário
-        if number < lower_value:
-            valor_mais_baixo = number
+print(f"Média das idades: {average_age:.2f}")
 
-# Exibe os valores mais altos e mais baixos
-print(f"Valor mais alto: {higher_value}")
-print(f"Valor mais baixo: {lower_value}")
+# Verifica quais idades estão acima ou abaixo da média
+above_average = [age for age in ages if age > average_age]
+below_average = [age for age in ages if age < average_age]
+
+print(f"Idades acima da média: {above_average}")
+print(f"Idades abaixo da média: {below_average}")
